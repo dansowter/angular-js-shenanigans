d5dbfb68235599ae29a2df9cd3cb163c480377c7
Dan Sowter <dan@netengine.com.au>
2013-02-15 13:15:42 +1000

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estimate::Application.config.secret_token = 'ed98df76ca744a285346503843949432433e125ac7ce2aff5b681ad93a00e3f0a5d5d9a9acd9ff576235bc02fd10fa2a662d92c960ea940adc6b106cb4106c16'
